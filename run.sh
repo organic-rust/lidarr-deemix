8553ab2d21db38a1ce5fb1c8d65495419f325f38
natasha <natasha@Natasha-Plex>
2025-08-04 16:25:47 +1200

    Wait for log files to exist before running tail

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ nohup python ./python/deemix-server.py > ~/nohup_deemix.txt 2>&1 &
 nohup pnpm run start > ~/nohup_server.txt 2>&1 &
 nohup mitmdump -s ./python/http-redirect-request.py > ~/nohup_mitmdump.txt 2>&1 &
 
+sleep 5
+
 tail -f ~/nohup_*.txt
